c58ec33f7b0d8b4184a5985bb98192f99b0ce07c
mrammars <ammar.saifulbahri96@gmail.com>
2022-01-11 05:04:32 +0800

    fix(update): add link and navigation inertia

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,13 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Home');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+}); 
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
 });
